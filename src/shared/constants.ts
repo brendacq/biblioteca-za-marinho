df2d64678e77f6e2c96255fb20f050ef653bc3e7
Brenda <breeeendaqn@gmail.com>
2023-02-13 23:11:01 -0300

    feature: Create Enums for categories

---
+++ src/shared/constants.ts
@@ -0,0 +1,18 @@
+export enum LITERARY_WORKS_CATEGORY {
+  book = 'Livro',
+  periodical = 'Periódico',
+  magazine = 'Revista',
+  didactic_note = 'Nota didática',
+  newspaper = 'Jornal',
+  technical_report = 'Relatório técnico',
+  doctoral_thesis = 'Tese de doutorado',
+  masters_dissertation = 'Dissertação de mestrado',
+}
+
+export enum READER_CATEGORY {
+  gradStudent = 'Aluno de graduação',
+  postGradStudent = 'Aluno de pós-graduação',
+  professor = 'Professor',
+  employee = 'Funcionário',
+  external_user = 'Usuário externo',
+}
