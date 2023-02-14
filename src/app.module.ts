21470cf1262102179f1376121b85393018fdcc4a
Brenda <breeeendaqn@gmail.com>
2023-02-13 23:08:44 -0300

    build: Setup MongoDB

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,25 +2,22 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ReadersModule } from './readers/readers.module';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule } from '@nestjs/config';
 import { LiteraryWorksModule } from './literary-works/literary-works.module';
+import { LiteraryCategoryModule } from './literary-category/literary-category.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ReaderCategoryModule } from './reader-category/reader-category.module';
+import { EmployeesModule } from './employees/employees.module';
 
 @Module({
   imports: [
     ReadersModule,
     ConfigModule.forRoot(),
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: process.env.MYSQL_DATABASE_HOST,
-      port: parseInt(process.env.MYSQL_DATABASE_PORT),
-      database: process.env.MYSQL_DATABASE,
-      username: process.env.MYSQL_DATABASE_USER,
-      password: process.env.MYSQL_DATABASE_PASSWORD,
-      entities: ['dist/**/*.entity.js'],
-      synchronize: true,
-    }),
+    MongooseModule.forRoot(process.env.DATABASE_URL),
     LiteraryWorksModule,
+    LiteraryCategoryModule,
+    ReaderCategoryModule,
+    EmployeesModule,
   ],
   controllers: [AppController],
   providers: [AppService],
