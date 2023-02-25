cc55e085ee650e83baabd68cf3b303fd596d911b
Brenda <breeeendaqn@gmail.com>
2023-02-24 23:08:27 -0300

    Remove useless import

--- src/literary-category/literary-category.repository.ts
+++ src/literary-category/literary-category.repository.ts
@@ -7,7 +7,6 @@ import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { LiteraryCategory } from './entities/literary-category.entity';
 import { LiteraryCategoryDocument } from './entities/literary-category.entity';
-import { NotFoundError } from 'rxjs';
 
 @Injectable()
 export class LiteraryCategoryRepository {
